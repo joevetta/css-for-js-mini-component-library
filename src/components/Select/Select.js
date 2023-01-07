79e0db6e8fe1136c800e71cd597eab566eca12ab
Joe Vetta <joe.vetta@gmail.com>
2023-01-07 10:24:23 -0600

    Select Component

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,81 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
-const Select = ({ label, value, onChange, children }) => {
+const Select = ({ id, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect id={id} value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ "--size": 24 + "px" }}>
+          <Icon id="chevron-down" strokeWidth={2} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  /* Allow the select to span the full height in Safari */
+  -webkit-appearance: none;
+`;
+
+const PresentationalBit = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  font-size: ${16 / 16}rem;
+  padding: 12px 16px;
+  padding-right: 52px;
+  border-radius: 8px;
+  /*
+    Selector: When the NativeSelect element is focused and 
+    the current element (PresentationalBit) is its sibling
+    then apply the rule to the current element
+
+    https://developer.mozilla.org/en-US/docs/Web/CSS/Adjacent_sibling_combinator
+  */
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212121; /* Fallback for non-webkit browsers */
+    outline: 5px auto -webkit-focus-ring-color; /* Setting for webkit browsers*/
+  }
+  /*
+    Selector: When the NativeSelect element is hovered and 
+    the current element (PresentationalBit) is its sibling
+    then apply the rule to the current element
+  */
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  pointer-events: none; /* Even though this is in front it cannot receive a click */
+`;
 
 export default Select;
